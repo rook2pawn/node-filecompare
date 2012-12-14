5abdcc0d7e7e7aacfd2bf2be6b9111122ab5ce32
David Wee <rook2pawn@gmail.com>
2012-12-14 02:57:54 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var lib = require('./lib');
+var path1 = './a.txt';
+var path2 = './b.txt';
+
+/*
+var f1 = lib.file(path1);
+var f2 = lib.file(path2);
+
+lib.compare(f1,f2,function(isEqual,success,failure) {
+    if (isEqual) 
+        success()
+    else 
+        failure()
+});
+*/
+exports = module.exports = function() {
+}
