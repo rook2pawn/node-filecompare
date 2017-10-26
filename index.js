751c6ffadcca267766f6af3b2321edf16482eda5
rook2pawn <rook2pawn@gmail.com>
2017-10-26 02:03:31 -0700

    more tests

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const lib = require('./lib');
 const path = require('path');
 
-const compare = function (path1,path2,cb) {
+const compare = function (path1,path2,cb,step,bufferSize) {
 
-  var f1 = lib.fileBufferObject(path1);
-  var f2 = lib.fileBufferObject(path2);
-  return lib.compare(f1,f2,cb);
+  step = step || 1;
+  bufferSize = bufferSize || 13;
+
+  var f1 = lib.fileBufferObject(path1,bufferSize);
+  var f2 = lib.fileBufferObject(path2,bufferSize);
+  console.log(f1,f2);
+  return lib.compare(f1,f2,cb,step,bufferSize);
 };
 
 exports = module.exports = compare;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,47 @@
-var test = require('tape');
+ var test = require('tape');
 var fc = require('../');
-var path = require('path')
+var path = require('path');
+
+test('when both files fit in first buffer', function(t) {
+
+
+  test('test for equality',function(t) {
+    t.plan(1);
+    fc(path.join(__dirname,'file1.txt'),path.join(__dirname,'file3.txt'),function(isEqual) {
+      t.equal(true,isEqual);
+    },1,14);
+  });
+
+  test('test for inequality',function(t) {
+    t.plan(1);
+    fc(path.join(__dirname,'file1.txt'),path.join(__dirname,'file2.txt'),function(isEqual) {
+      t.equal(false,isEqual);
+    },1,14);
+  });
+
+  t.end();
+});
+
+test('when both files are one larger than buffer', function(t) {
+
+
+  test('test for equality',function(t) {
+    t.plan(1);
+    fc(path.join(__dirname,'file1.txt'),path.join(__dirname,'file3.txt'),function(isEqual) {
+      t.equal(true,isEqual);
+    },1,13);
+  });
+
+  test('test for inequality',function(t) {
+    t.plan(1);
+    fc(path.join(__dirname,'file1.txt'),path.join(__dirname,'file2.txt'),function(isEqual) {
+      t.equal(false,isEqual);
+    },1,13);
+  });
+
+  t.end();
+});
+
 
 test('test same filesize', function(t) {
 
